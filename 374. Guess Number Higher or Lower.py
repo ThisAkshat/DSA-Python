5b8a8f9e8e6f0498940fd1842fd3b20053b938d1
Akshat Sharma <akshatsharma1313@gmail.com>
2026-09-05 16:35:58 +0530

    Add solution for Guess Number Higher or Lower
    Implement binary search to guess the number.

---
+++ 374. Guess Number Higher or Lower.py
@@ -0,0 +1,44 @@
+class Solution(object):
+    def guessNumber(self, n):
+        low = 1
+        high = n
+        while(low<=high):
+            mid = low + (high - low)/2
+            result = guess(mid)
+            if result ==0:
+                return mid
+            elif result < 0:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return -1
+
+  """
+374. Guess Number Higher or Lower
+
+We are playing the Guess Game. The game is as follows:
+I pick a number from 1 to n. You have to guess which number I picked (the number I picked stays the same throughout the game).
+Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+You call a pre-defined API int guess(int num), which returns three possible results:
+-1: Your guess is higher than the number I picked (i.e. num > pick).
+1: Your guess is lower than the number I picked (i.e. num < pick).
+0: your guess is equal to the number I picked (i.e. num == pick).
+Return the number that I picked.
+ 
+
+Example 1:
+Input: n = 10, pick = 6
+Output: 6
+
+Example 2:
+Input: n = 1, pick = 1
+Output: 1
+
+Example 3:
+Input: n = 2, pick = 1
+Output: 1
+
+Constraints:
+1 <= n <= 231 - 1
+1 <= pick <= n
+  """

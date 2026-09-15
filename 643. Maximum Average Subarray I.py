96a8cd279707ea52b829a12bdf7da85fa7146288
Akshat Sharma <akshatsharma1313@gmail.com>
2026-09-15 16:02:25 +0530

    Add solution for Maximum Average Subarray I
    Implemented a solution to find the maximum average of a contiguous subarray of length k. The function calculates the average using a sliding window approach.

---
+++ 643. Maximum Average Subarray I.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def findMaxAverage(self, nums, k):
+        n = len(nums)
+        
+        currentSum = sum(nums[0:k])         # pehli window ka sum, ek baar nikala
+        Max = float(currentSum) / k
+        
+        for i in range(1, n-k+1):
+            currentSum = currentSum - nums[i-1] + nums[i+k-1]   # slide karo
+            Avg = float(currentSum) / k
+            Max = max(Max, Avg)
+        
+        return Max
+
+  """
+643. Maximum Average Subarray I
+
+You are given an integer array nums consisting of n elements, and an integer k.
+Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value.
+Any answer with a calculation error less than 10-5 will be accepted.
+ 
+
+Example 1:
+Input: nums = [1,12,-5,-6,50,3], k = 4
+Output: 12.75000
+Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+
+Example 2:
+Input: nums = [5], k = 1
+Output: 5.00000
+  """
